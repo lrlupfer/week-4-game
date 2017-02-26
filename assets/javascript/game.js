d3237c6aae29523dffc958b645b7d888125a8681
Lindsey Lupfer <lindsey@Lindseys-MacBook-Pro.local>
2017-02-25 20:55:21 -0500

    first attempt at crystals collector game

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+// Global variables
+
+var gameScore = 0;
+
+var gem1 = Math.floor((Math.random() * 19) + 1);;
+var gem2 = Math.floor((Math.random() * 19) + 1);;
+var gem3 = Math.floor((Math.random() * 19) + 1);;
+var gem4 = Math.floor((Math.random() * 19) + 1);;
+
+var winCount = 0;
+var lossCount = 0;
+
+
+	$( "#gem1" ).click(function() {
+
+  		alert(gem1);
+
+	});
+
+
+
+});
